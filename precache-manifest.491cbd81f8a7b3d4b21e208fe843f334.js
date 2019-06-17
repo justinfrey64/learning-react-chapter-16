332ee96634ebb7731ee45595981a8f7d3eea889f
Justin Frey <justinfrey64@gmail.com>
2019-06-17 13:40:24 -0400

    Updates

---
+++ precache-manifest.491cbd81f8a7b3d4b21e208fe843f334.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5209fdc4e6ef536d6b8821d4e53fe247",
+    "url": "/learning-react-chapter-16/index.html"
+  },
+  {
+    "revision": "5371df44b150c46abadf",
+    "url": "/learning-react-chapter-16/static/css/main.6da9adf3.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/learning-react-chapter-16/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "5371df44b150c46abadf",
+    "url": "/learning-react-chapter-16/static/js/main.cffef099.chunk.js"
+  },
+  {
+    "revision": "a80db168b0602b0d2ce6",
+    "url": "/learning-react-chapter-16/static/js/runtime~main.77651df6.js"
+  }
+]);
